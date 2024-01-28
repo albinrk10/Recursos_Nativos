c4ed156fbd1d3e1470edc9b5c4f0377b20715c66
Albin Anthony Hinostroza <79820950+albinrk10@users.noreply.github.com>
2024-01-28 12:47:16 -0500

    permission_handler

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,16 @@
+import 'package:go_router/go_router.dart';
+
+import '../../presentation/screens/screen.dart';
+
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/permissions',
+      builder: (context, state) => const PermissionsScreen(),
+    ),
+
+  ]);
